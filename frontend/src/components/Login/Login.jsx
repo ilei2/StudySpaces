659fe584b7be9c36c1e54daa1c58ca353405fd70
kirstenwong <kewong2@illinois.edu>
2017-12-04 17:47:34 -0600

    added login and signup components

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './Login.scss';
+
+
+class Login extends Component {
+
+
+  render() {
+    return (
+      <h1>Login Component</h1>
+    )
+  }
+}
+
+export default Login
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,10 +6,16 @@ import {
   Route,
   Link} from 'react-router-dom'
 
+import Login from './components/Login/Login.jsx';
+import Signup from './components/Signup/Signup.jsx';
+
   render(
     <Router>
       <div>
         <h1>Study Spaces</h1>
+
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup}/>
       </div>
     </Router>, document.getElementById('app')
   );
