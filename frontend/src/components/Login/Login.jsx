71450ffbf8f745a2769cc82e3e3a644a11ea09a6
kirstenwong <kewong2@illinois.edu>
2017-12-05 00:49:20 -0600

    working on login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,13 +1,70 @@
-'use strict'
 import React, { Component } from 'react'
+import { Button, Input, Card } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import axios from 'axios'
 
 import styles from './Login.scss';
 
+
 class Login extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      user: {
+        password: '',
+        email: ''
+      },
+
+      message: ''
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const email = encodeURIComponent(this.state.user.email);
+    const password = encodeURIComponent(this.state.user.password);
+    const formData = `email=${email}&password=${password}`;
+  }
+
+  onChangeEmail(e) {
+    const user = this.state.user;
+    user.email = e.target.value;
+    this.setState({user})
+  }
+
+  onChangePassword(e) {
+    const user = this.state.user;
+    user.password = e.target.value;
+    this.setState({user})
+  }
+
+
   render() {
     return (
-      <h1>Login Component</h1>
+      <form className="Login" action="/" onSubmit={this.onSubmit}>
+        <Card className="Login__content">
+          <div>
+            <h1>Login</h1>
+            <Input label="Email" onChange={this.onChangeEmail} />
+            <br/><br/>
+            <Input label="Password" onChange={this.onChangePassword} />
+            <br/><br/>
+
+            <p>{this.state.message}</p>
+            <Input type="submit" />
+            <h4>No account? Sign up <Link to="/register">here</Link></h4>
+
+          </div>
+        </Card>
+      </form>
     )
   }
 }
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,10 +1,12 @@
-'use strict'
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './Signup.scss';
 
+
 class Signup extends Component {
+
+
   render() {
     return (
       <h1>Signup Component</h1>
