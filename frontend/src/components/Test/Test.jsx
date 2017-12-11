a3c5add443d01a209d61c27a9e4fb58c00316c08
Jasmine Kwon <jhkwon3@illinois.edu>
2017-12-11 00:43:06 -0600

    test page for listview

--- frontend/src/components/Test/Test.jsx
+++ frontend/src/components/Test/Test.jsx
@@ -1,20 +1,126 @@
 'use strict'
 import React, { Component } from 'react';
-import { Header, Image, Card, Icon, Table, Rating, Dropdown, List } from 'semantic-ui-react'
+import {
+  Button,
+  Search,
+  List,
+  Image,
+  Input,
+  Dropdown,
+  Form,
+  Checkbox,
+  Menu,
+  Icon
+ }
+from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-
 import styles from './Test.scss';
-import NavBar from '../NavBar/NavBar.jsx';
+import TopBar from '../TopBar/TopBar.jsx';
+import PropTypes from 'prop-types';
+import axios from 'axios'
+
 
 class Test extends Component {
-  render() {
-    return (
-      <div className="ui container center aligned">
-        <NavBar/>
-        <h1>Test Component</h1>
-        </div>
-      )
-    }
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: "",
+      search: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
   }
 
-  export default Test
+  handleChange(e, {value}) {
+    this.setState({
+      value
+    });
+  }
+
+  handleSearch(e, {value}) {
+    this.setState({
+      search: value
+    })
+  }
+
+  submitSearch() {
+    console.log("radio selection:");
+    console.log(this.state.value);
+    console.log("Search text:");
+    console.log(this.state.search);
+  /*  var url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=uiuc&key=AIzaSyBDRH-amNMHWJgOXGFuASOFP7x7EOihKF0";
+    axios.get(url).then((response) => {
+      console.log(response.data)
+    });
+
+*/
+  }
+
+  render() {
+    return (
+      <div>
+        <TopBar/>
+        <h1>Test Component</h1>
+        <Input
+          placeholder='i.e. Cafes near me'
+          size='massive'
+          onChange={this.handleSearch}
+        />
+          <Form className="ui home-form">
+            <Form.Field>
+              <Checkbox
+                radio
+                label="quiet"
+                name="first"
+                value='quiet'
+                checked={this.state.value === 'quiet'}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox
+                radio
+                label="busy"
+                name="second"
+                value='busy'
+                checked={this.state.value === 'busy'}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox
+                radio
+                label="wifi"
+                name="third"
+                value='wifi'
+                checked={this.state.value === 'wifi'}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox
+                radio
+                label="group"
+                name="fourth"
+                value='group'
+                checked={this.state.value === 'group'}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+          </Form>
+          <Button
+            color="grey"
+            className="ui home-submit-btn"
+            onClick={this.submitSearch}
+          >
+              Submit
+          </Button>
+      </div>
+    )
+  }
+}
+
+export default Test
